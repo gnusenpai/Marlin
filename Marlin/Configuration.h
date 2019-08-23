059cbceb92345c76f735cb6abaef08375de9c479
pspslimboy <pspslimboy98@gmail.com>
2019-08-23 12:32:19 -0400

    gnu: Set bed dimensions

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1369,8 +1369,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 231
+#define Y_BED_SIZE 204
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1378,6 +1378,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
+<<<<<<< HEAD
 #define Z_MAX_POS 200
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
@@ -1385,6 +1386,9 @@
 //#define J_MAX_POS 50
 //#define K_MIN_POS 0
 //#define K_MAX_POS 50
+=======
+#define Z_MAX_POS 300
+>>>>>>> 5713b48cc7 (gnu: Set bed dimensions)
 
 /**
  * Software Endstops
